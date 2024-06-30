034ef7909b27d4edde99235e97c007dd1fbc122d
Sebastian <mrseb10@gmail.com>
2024-06-30 22:44:13 +0200

    EF Core, Database and tables configuration

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,13 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<FriendsDbContext>(options =>
+  options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
---
+++ Data/FriendsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Data.Configurations;
+using Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class FriendsDbContext(DbContextOptions options) : DbContext(options)
+{
+  public DbSet<Friend> Friends { get; set; }
+  public DbSet<Category> Categories { get; set; }
+  
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.ApplyConfiguration(new FriendsConfiguration());
+    modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+    base.OnModelCreating(modelBuilder);
+  }
+}
