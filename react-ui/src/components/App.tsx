d95d7b56b706f1932aeadb6447d92894f26ae38f
Sebastian <mrseb10@gmail.com>
2024-07-03 11:57:31 +0200

    Input validation

--- react-ui/src/components/App.tsx
+++ react-ui/src/components/App.tsx
@@ -5,19 +5,19 @@ import EditFriendDialog from "./EditFriendDialog";
 import FriendItem from "@/components/FriendItem";
 
 const App = () => {
-    const {friends, isLoading, error, deleteFriend} = useFriends();
+    const {friends, isLoading, error} = useFriends();
 
     return (
         <div>
             {isLoading && <p>Loading...</p>}
             {error && <p>{error}</p>}
             <div className="flex flex-col gap-8">
-                <EditFriendDialog/>
+                {friends && <EditFriendDialog/>}
                 {friends && friends.length > 0 ? (
                     friends.map((f) => <FriendItem key={f.id} friend={f}/>)
-                ) : (
+                ) : friends && friends.length === 0 ? (
                     <p className="text-white">No friends found. Add a new friend to get started!</p>
-                )}
+                ) : null}
             </div>
         </div>
     );
